e70d198c6a3167b3bf72bf1888633c7f15547028
Dane Cronin <danecronin@gmail.com>
2024-11-18 17:41:08 -0700

    initial commit new install

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <main className="bg-slate-300/20 h-full">
+        CHROMASCAPES
+  </main>
+  
+      
+  );
+}
+
+export default App;
